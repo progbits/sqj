ba8976895e0218bb5f9133ce95ffdd0caae50358
progbits <53880655+progbits@users.noreply.github.com>
2020-12-28 20:47:36 +0000

    Apply .clang-format style to test_json_tokenize.c

--- test/test_json_tokenize.c
+++ test/test_json_tokenize.c
@@ -1,43 +1,44 @@
-#include <stdlib.h>
-#include <stddef.h>
 #include <check.h>
+#include <stddef.h>
+#include <stdlib.h>
 
 #include "../src/json_tokenize.h"
 
-START_TEST(test_json_tokenize_0)
-{
+START_TEST(test_json_tokenize_0) {
     // Arrange.
-    const char *raw_json = "{\"foo\": 1, \"bar\": 3.14, \"baz\": true, \"foobar\": false, \"foobaz\": \"hello, world\"}";
+    const char* raw_json = "{\"foo\": 1, \"bar\": 3.14, \"baz\": true, "
+                           "\"foobar\": false, \"foobaz\": \"hello, world\"}";
     Token* tokens = NULL;
     size_t n_tokens = 0;
     tokenize(raw_json, &tokens, &n_tokens);
 
     const JSON_TOKEN expected_tokens[] = {
-            JSON_TOKEN_LEFT_CURLY_BRACKET,
-            JSON_TOKEN_STRING,
-            JSON_TOKEN_COLON,
-            JSON_TOKEN_NUMBER,
-            JSON_TOKEN_COMMA,
-            JSON_TOKEN_STRING,
-            JSON_TOKEN_COLON,
-            JSON_TOKEN_NUMBER,
-            JSON_TOKEN_COMMA,
-            JSON_TOKEN_STRING,
-            JSON_TOKEN_COLON,
-            JSON_TOKEN_TRUE,
-            JSON_TOKEN_COMMA,
-            JSON_TOKEN_STRING,
-            JSON_TOKEN_COLON,
-            JSON_TOKEN_FALSE,
-            JSON_TOKEN_COMMA,
-            JSON_TOKEN_STRING,
-            JSON_TOKEN_COLON,
-            JSON_TOKEN_STRING,
-            JSON_TOKEN_RIGHT_CURLY_BRACKET,
+        JSON_TOKEN_LEFT_CURLY_BRACKET,
+        JSON_TOKEN_STRING,
+        JSON_TOKEN_COLON,
+        JSON_TOKEN_NUMBER,
+        JSON_TOKEN_COMMA,
+        JSON_TOKEN_STRING,
+        JSON_TOKEN_COLON,
+        JSON_TOKEN_NUMBER,
+        JSON_TOKEN_COMMA,
+        JSON_TOKEN_STRING,
+        JSON_TOKEN_COLON,
+        JSON_TOKEN_TRUE,
+        JSON_TOKEN_COMMA,
+        JSON_TOKEN_STRING,
+        JSON_TOKEN_COLON,
+        JSON_TOKEN_FALSE,
+        JSON_TOKEN_COMMA,
+        JSON_TOKEN_STRING,
+        JSON_TOKEN_COLON,
+        JSON_TOKEN_STRING,
+        JSON_TOKEN_RIGHT_CURLY_BRACKET,
     };
 
     // Assert.
-    ck_assert_int_eq(n_tokens, sizeof(expected_tokens) / sizeof(expected_tokens[0]));
+    ck_assert_int_eq(n_tokens,
+                     sizeof(expected_tokens) / sizeof(expected_tokens[0]));
     for (size_t i = 0; i < n_tokens; i++) {
         ck_assert_int_eq(tokens[i].type, expected_tokens[i]);
     }
@@ -45,36 +46,36 @@ START_TEST(test_json_tokenize_0)
 }
 END_TEST
 
-START_TEST(test_json_tokenize_1)
-{
-    const char *raw_json = "{\"foo\": [{\"bar\": 1}, {\"baz\": 2}]}";
+START_TEST(test_json_tokenize_1) {
+    const char* raw_json = "{\"foo\": [{\"bar\": 1}, {\"baz\": 2}]}";
 
     Token* tokens = NULL;
     size_t n_tokens = 0;
     tokenize(raw_json, &tokens, &n_tokens);
 
     const JSON_TOKEN expected_tokens[] = {
-            JSON_TOKEN_LEFT_CURLY_BRACKET,
-            JSON_TOKEN_STRING,
-            JSON_TOKEN_COLON,
-            JSON_TOKEN_LEFT_SQUARE_BRACKET,
-            JSON_TOKEN_LEFT_CURLY_BRACKET,
-            JSON_TOKEN_STRING,
-            JSON_TOKEN_COLON,
-            JSON_TOKEN_NUMBER,
-            JSON_TOKEN_RIGHT_CURLY_BRACKET,
-            JSON_TOKEN_COMMA,
-            JSON_TOKEN_LEFT_CURLY_BRACKET,
-            JSON_TOKEN_STRING,
-            JSON_TOKEN_COLON,
-            JSON_TOKEN_NUMBER,
-            JSON_TOKEN_RIGHT_CURLY_BRACKET,
-            JSON_TOKEN_RIGHT_SQUARE_BRACKET,
-            JSON_TOKEN_RIGHT_CURLY_BRACKET,
+        JSON_TOKEN_LEFT_CURLY_BRACKET,
+        JSON_TOKEN_STRING,
+        JSON_TOKEN_COLON,
+        JSON_TOKEN_LEFT_SQUARE_BRACKET,
+        JSON_TOKEN_LEFT_CURLY_BRACKET,
+        JSON_TOKEN_STRING,
+        JSON_TOKEN_COLON,
+        JSON_TOKEN_NUMBER,
+        JSON_TOKEN_RIGHT_CURLY_BRACKET,
+        JSON_TOKEN_COMMA,
+        JSON_TOKEN_LEFT_CURLY_BRACKET,
+        JSON_TOKEN_STRING,
+        JSON_TOKEN_COLON,
+        JSON_TOKEN_NUMBER,
+        JSON_TOKEN_RIGHT_CURLY_BRACKET,
+        JSON_TOKEN_RIGHT_SQUARE_BRACKET,
+        JSON_TOKEN_RIGHT_CURLY_BRACKET,
     };
 
     // Assert.
-    ck_assert_int_eq(n_tokens, sizeof(expected_tokens) / sizeof(expected_tokens[0]));
+    ck_assert_int_eq(n_tokens,
+                     sizeof(expected_tokens) / sizeof(expected_tokens[0]));
     for (size_t i = 0; i < n_tokens; i++) {
         ck_assert_int_eq(tokens[i].type, expected_tokens[i]);
     }
@@ -84,10 +85,10 @@ START_TEST(test_json_tokenize_1)
 }
 END_TEST
 
-Suite *tokenizer_suite(void) {
-    Suite *s = suite_create("json_tokenize");
+Suite* tokenizer_suite(void) {
+    Suite* s = suite_create("json_tokenize");
 
-    TCase *tc_core = tcase_create("Core");
+    TCase* tc_core = tcase_create("Core");
     tcase_add_test(tc_core, test_json_tokenize_0);
     tcase_add_test(tc_core, test_json_tokenize_1);
 
@@ -97,8 +98,8 @@ Suite *tokenizer_suite(void) {
 
 int main(void) {
     int number_failed;
-    Suite *s;
-    SRunner *sr;
+    Suite* s;
+    SRunner* sr;
 
     s = tokenizer_suite();
     sr = srunner_create(s);
